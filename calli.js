24fa1e1f1c247e5db2cbd91d434fdc446818aeed
X <91824551+MrTheX@users.noreply.github.com>
2021-11-16 22:21:03 +0300

    Update calli.js

--- calli.js
+++ calli.js
@@ -66,7 +66,7 @@ calli.on("message", message => {
   }
 });
 ///////////////////////////////////////////////////////////////////////////////
-calli.login("OTEwMjQ1NzE5MjU0OTE3MTIw.YZQCYw.3D9AzIcSbSKzZeQ94_SgzerDJHg");
+calli.login("OTEwMjQ1NzE5MjU0OTE3MTIw.YZQCYw.3D9AzIcSbSKzZeQ94_SgzerDJHg")
 ///////////////////////////////////////////////////////////////////////////////
 const callienabled  = "";
 const callidisabled = "";
